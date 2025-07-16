dc6f1401a4483797542c34c111dc5aab1db3d483
xiejinlong <xiejinlong@huya.com>
2025-07-16 14:28:51 +0800

    feat: 版本3

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const Lazy = lazy(() => import('./Lazy'))
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const [count2, setCount2] = useState(0)
 
   useEffect(() => {
     window.__init_istanbul_widget__?.()
@@ -16,6 +16,9 @@ function App() {
     <>
       <div className='card'>
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+      </div>
+      <div className='card'>
+        <button onClick={() => setCount2((count) => count + 1)}>count2 is {count2}</button>
       </div>
       <Suspense fallback={<div />}>
         <Lazy />
